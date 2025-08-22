8b5daf39c76ebe6129f015ac5a891a04ca7172c0
Shmar10 <shamar@d219.org>
2025-08-22 15:10:50 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Offline cache with version bump
-const CACHE = 'sdc-v6';
+const CACHE = 'sdc-v7';
 const ASSETS = ['./','index.html','manifest.webmanifest','sw.js','icons/icon-192.png','icons/icon-512.png'];
 
 self.addEventListener('install', e => {
